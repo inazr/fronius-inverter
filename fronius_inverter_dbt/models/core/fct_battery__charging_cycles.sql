3645a0dd5a1d2f5ab47cd8c0586562c542031f1d
Stephan Ohm <github@stephan-ohm.com>
2023-01-23 20:56:29 +0100

    added fct_battery__charging_cycles.sql

---
+++ fronius_inverter_dbt/models/core/fct_battery__charging_cycles.sql
@@ -0,0 +1,10 @@
+SELECT
+        DATE(int_battery__charge_diff.Head_Timestamp, 'Europe/Berlin') AS reporting_date_cest,
+        SUM(CASE WHEN int_battery__charge_diff.akku_charge_diff > 0 THEN int_battery__charge_diff.akku_charge_diff ELSE 0 END) AS percent_loading,
+        SUM(CASE WHEN int_battery__charge_diff.akku_charge_diff < 0 THEN int_battery__charge_diff.akku_charge_diff ELSE 0 END) AS percent_unloading,
+        SUM(CASE WHEN int_battery__charge_diff.akku_charge_diff > 0 THEN int_battery__charge_diff.akku_charge_diff ELSE 0 END) / 100 AS full_charging_cycle
+FROM
+        {{ ref('int_battery__charge_diff') }}
+        --transformation.int_battery__charge_diff
+GROUP BY
+        1
--- fronius_inverter_dbt/models/transformation/int_battery__charge_diff.sql
+++ fronius_inverter_dbt/models/transformation/int_battery__charge_diff.sql
@@ -1,6 +1,8 @@
 SELECT
         Head_Timestamp,
         -- Returns the difference between the Head_Timestamp and the FOLLOWING value
+        -- charging -> +
+        -- discharging -> -
         LAG(akku_charge_pct, 1) OVER (ORDER BY Head_Timestamp DESC) - akku_charge_pct AS akku_charge_diff,
 FROM
         {{ ref('stg_fronius__get_power_flow_realtime_data') }}
