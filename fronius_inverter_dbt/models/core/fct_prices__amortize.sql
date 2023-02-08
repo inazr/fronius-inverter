3a2285666c76f7a4f64eee0aa5aebbde681fa132
Stephan Ohm <github@stephan-ohm.com>
2023-02-08 17:06:37 +0100

    created fct_prices__amortize.sql

---
+++ fronius_inverter_dbt/models/core/fct_prices__amortize.sql
@@ -0,0 +1,24 @@
+WITH aggregates AS (
+
+    SELECT
+    --         fct_prices__electricity.reporting_date,
+            AVG(fct_prices__electricity.Eigenverbrauchabgabe)
+          + AVG(fct_prices__electricity.Opportunitaetserloes)
+          + AVG(fct_prices__electricity.Einspeiseverguetung) AS avg_income_per_day,
+            SUM(DISTINCT seed_price_onetime.price_netto) AS total_costs,
+    FROM
+            {{ ref('fct_prices__electricity') }}
+--             core.fct_prices__electricity
+    CROSS JOIN
+            {{ ref('seed_price_onetime') }}
+--             seeds.seed_price_onetime
+
+)
+
+SELECT
+        avg_income_per_day,
+        total_costs,
+        total_costs / avg_income_per_day AS num_of_days_to_amortize,
+        total_costs / avg_income_per_day / 365 AS num_of_years_to_amortize,
+FROM
+        aggregates
