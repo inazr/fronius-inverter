390c58a412841370234df4ed0a828f4bd356efc5
inazr <github@lentzen.email>
2023-02-11 21:08:25 +0100

    switched 24 to count hours with data

--- fronius_inverter_dbt/models/core/fct_fronius__app_data_daily.sql
+++ fronius_inverter_dbt/models/core/fct_fronius__app_data_daily.sql
@@ -3,17 +3,17 @@
 SELECT
         DATE(Head_Timestamp, 'Europe/Berlin') AS reporting_date,
         -- Produktion
-        AVG(pv_watt) * 24 / 1000 AS Produktion,
-        (AVG(pv_watt) + AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END)) * 24 / 1000 AS Eigenverbrauch,
-        ((AVG(pv_watt) + AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END)) + AVG(CASE WHEN akku_watt < 0 THEN akku_watt ELSE 0 END)) * 24 / 1000 AS Direktverbrauch,
-        AVG(CASE WHEN akku_watt < 0 THEN akku_watt ELSE 0 END) * 24 / 1000 * -1 AS in_die_Batterie,
-        AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END)  * 24 / 1000 * -1 AS Netzeinspeisung,
+        AVG(pv_watt) * COUNT(DISTINCT EXTRACT(HOUR FROM DATETIME(Head_Timestamp, 'Europe/Berlin'))) / 1000 AS Produktion,
+        (AVG(pv_watt) + AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END)) * COUNT(DISTINCT EXTRACT(HOUR FROM DATETIME(Head_Timestamp, 'Europe/Berlin'))) / 1000 AS Eigenverbrauch,
+        ((AVG(pv_watt) + AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END)) + AVG(CASE WHEN akku_watt < 0 THEN akku_watt ELSE 0 END)) * COUNT(DISTINCT EXTRACT(HOUR FROM DATETIME(Head_Timestamp, 'Europe/Berlin'))) / 1000 AS Direktverbrauch,
+        AVG(CASE WHEN akku_watt < 0 THEN akku_watt ELSE 0 END) * COUNT(DISTINCT EXTRACT(HOUR FROM DATETIME(Head_Timestamp, 'Europe/Berlin'))) / 1000 * -1 AS in_die_Batterie,
+        AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END)  * COUNT(DISTINCT EXTRACT(HOUR FROM DATETIME(Head_Timestamp, 'Europe/Berlin'))) / 1000 * -1 AS Netzeinspeisung,
 
         -- Verbrauch
-        (AVG(pv_watt) + AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END) + AVG(CASE WHEN akku_watt < 0 THEN akku_watt ELSE 0 END) + AVG(CASE WHEN akku_watt > 0 THEN akku_watt ELSE 0 END) + AVG(CASE WHEN grid_watt > 0 THEN grid_watt ELSE 0 END)) * 24 / 1000 AS Verbrauch,
-        (AVG(pv_watt) + AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END) + AVG(CASE WHEN akku_watt < 0 THEN akku_watt ELSE 0 END) + AVG(CASE WHEN akku_watt > 0 THEN akku_watt ELSE 0 END)) * 24 / 1000  AS Eigenerzeugung,
-        AVG(CASE WHEN akku_watt > 0 THEN akku_watt ELSE 0 END) * 24 / 1000 AS aus_der_Batterie,
-        AVG(CASE WHEN grid_watt > 0 THEN grid_watt ELSE 0 END)  * 24 / 1000 AS Netzbezug,
+        (AVG(pv_watt) + AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END) + AVG(CASE WHEN akku_watt < 0 THEN akku_watt ELSE 0 END) + AVG(CASE WHEN akku_watt > 0 THEN akku_watt ELSE 0 END) + AVG(CASE WHEN grid_watt > 0 THEN grid_watt ELSE 0 END)) * COUNT(DISTINCT EXTRACT(HOUR FROM DATETIME(Head_Timestamp, 'Europe/Berlin'))) / 1000 AS Verbrauch,
+        (AVG(pv_watt) + AVG(CASE WHEN grid_watt < 0 THEN grid_watt ELSE 0 END) + AVG(CASE WHEN akku_watt < 0 THEN akku_watt ELSE 0 END) + AVG(CASE WHEN akku_watt > 0 THEN akku_watt ELSE 0 END)) * COUNT(DISTINCT EXTRACT(HOUR FROM DATETIME(Head_Timestamp, 'Europe/Berlin'))) / 1000  AS Eigenerzeugung,
+        AVG(CASE WHEN akku_watt > 0 THEN akku_watt ELSE 0 END) * COUNT(DISTINCT EXTRACT(HOUR FROM DATETIME(Head_Timestamp, 'Europe/Berlin'))) / 1000 AS aus_der_Batterie,
+        AVG(CASE WHEN grid_watt > 0 THEN grid_watt ELSE 0 END)  * COUNT(DISTINCT EXTRACT(HOUR FROM DATETIME(Head_Timestamp, 'Europe/Berlin'))) / 1000 AS Netzbezug,
 
         -- Battery
         AVG(akku_charge_pct) AS Ladezustand
