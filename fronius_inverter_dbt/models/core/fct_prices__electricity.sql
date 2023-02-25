498e46597de534cb07383ef2fc570fefe5b9f714
stephanohm <github@stephan-ohm.com>
2023-02-25 11:45:10 +0100

    added Bilanz to fct_prices__electricity.sql

--- fronius_inverter_dbt/models/core/fct_prices__electricity.sql
+++ fronius_inverter_dbt/models/core/fct_prices__electricity.sql
@@ -1,7 +1,20 @@
+WITH base_cte AS (
+    SELECT
+            fct_fronius__app_data_daily.reporting_date,
+            CAST(fct_fronius__app_data_daily.Eigenverbrauch * (0.2399 - (0.2399 / 1.19)) * 0.19 * -1 AS NUMERIC) AS Eigenverbrauchabgabe, -- (Brutto Hausstrom - Netto Hausstrom) * MwSt * -1
+            fct_fronius__app_data_daily.Verbrauch * 0.2399 AS Opportunitaetserloes,
+            fct_fronius__app_data_daily.Netzeinspeisung * 0.082 AS Einspeiseverguetung,    --Anlagen bis 10 kWp erhalten 8,2 Cent pro kWh.
+    FROM
+            {{ ref('fct_fronius__app_data_daily') }}
+)
+
 SELECT
-        fct_fronius__app_data_daily.reporting_date,
-        CAST(fct_fronius__app_data_daily.Eigenverbrauch * (0.2399 - (0.2399 / 1.19)) * 0.19 * -1 AS NUMERIC) AS Eigenverbrauchabgabe, -- (Brutto Hausstrom - Netto Hausstrom) * MwSt * -1
-        fct_fronius__app_data_daily.Verbrauch * 0.2399 AS Opportunitaetserloes,
-        fct_fronius__app_data_daily.Netzeinspeisung * 0.082 AS Einspeiseverguetung,    --Anlagen bis 10 kWp erhalten 8,2 Cent pro kWh.
+        reporting_date,
+        Eigenverbrauchabgabe,
+        Opportunitaetserloes,
+        Einspeiseverguetung,
+        Eigenverbrauchabgabe
+      + Opportunitaetserloes
+      + Einspeiseverguetung AS Bilanz
 FROM
-        {{ ref('fct_fronius__app_data_daily') }}
+        base_cte
