e9316364e0675f057270836178cceae995fbcb1c
stephanohm <github@stephan-ohm.com>
2023-04-01 19:25:34 +0200

    added battery savings

--- fronius_inverter_dbt/models/reporting/rpt_grafana__aggregated_production.sql
+++ fronius_inverter_dbt/models/reporting/rpt_grafana__aggregated_production.sql
@@ -3,7 +3,8 @@ SELECT
         SUM(CASE WHEN fct_fronius__app_data_daily.reporting_date = CURRENT_DATE THEN Produktion END) AS Produktion_today,
         SUM(CASE WHEN fct_fronius__app_data_daily.reporting_date >= DATE_TRUNC(CURRENT_DATE, MONTH) THEN Produktion END) AS Produktion_month,
         SUM(CASE WHEN fct_fronius__app_data_daily.reporting_date >= DATE_TRUNC(CURRENT_DATE, YEAR) THEN Produktion END) AS Produktion_year,
-        SUM(Produktion) AS Produktion_alltime
-
+        SUM(Produktion) AS Produktion_alltime,
+        SUM(aus_der_Batterie) AS aus_der_Batterie_alltime,
+        SUM(aus_der_Batterie) * (0.2399 - 0.082) AS battery_savings_alltime,
 FROM
         {{ ref('fct_fronius__app_data_daily') }}
